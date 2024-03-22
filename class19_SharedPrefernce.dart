8beac277c1978b9678fc3497777a5234d67eca3c
yeowon0411 <dudnjsdl11@naver.com>
2024-03-23 02:37:24 +0800

    SharedPreference Study

--- class19_SharedPrefernce.dart
+++ class19_SharedPrefernce.dart
@@ -1,28 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
