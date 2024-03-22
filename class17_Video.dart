34c462679c069880fc37878fccfb1c0149127663
yeowon0411 <dudnjsdl11@naver.com>
2024-03-22 16:21:18 +0800

    Video Study

--- class17_Video.dart
+++ class17_Video.dart
@@ -1,26 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -33,7 +13,7 @@ class _MyHomePageState extends State<MyHomePage> {
   static String youtubeId = '1Sw0yFdnTWo';
 
   final YoutubePlayerController _con = YoutubePlayerController(initialVideoId: youtubeId,
-  flags: const YoutubePlayerFlags(autoPlay: false));
+      flags: const YoutubePlayerFlags(autoPlay: false));
   @override
   Widget build(BuildContext context) {
     return Scaffold(
